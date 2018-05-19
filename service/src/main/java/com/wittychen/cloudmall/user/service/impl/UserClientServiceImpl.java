c74f1b39b313ca698340b503aa8331b30c8ef245
chenfan <chenfan@guahao.com>
2018-05-19 19:21:34 +0800

    熔断

--- service/src/main/java/com/wittychen/cloudmall/user/service/impl/UserClientServiceImpl.java
+++ service/src/main/java/com/wittychen/cloudmall/user/service/impl/UserClientServiceImpl.java
@@ -5,36 +5,38 @@
  * You shall not disclose such Confidential Information and shall use it only
  * in accordance with the terms of the license agreement you entered into with GuaHao.com.
  */
-package com.wittychen.cloudmall.user.service;
+package com.wittychen.cloudmall.user.service.impl;
 
 import com.wittychen.cloudmall.user.api.bo.UserBO;
-import com.wittychen.cloudmall.user.api.service.UserService;
+import com.wittychen.cloudmall.user.api.service.UserClientService;
 import com.wittychen.cloudmall.user.manager.UserManger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 用户服务
+ * 这种方式web需要依赖服务的api
  *
  * @author chenfan
  * @version V1.0
- * @since 2018-05-14 18:06
+ * @since 2018-05-19 14:29
  */
-
-
 @RestController
-@RequestMapping("/user")
-public class UserServiceImpl implements UserService {
+public class UserClientServiceImpl implements UserClientService {
 
     @Autowired
     private UserManger userManger;
 
-    @GetMapping(value = "/{id}")
+    @Override
     public UserBO get(@PathVariable("id") Long id) {
         return userManger.get(id);
+    }
+
+    @Override
+    public UserBO getName(Long id) {
+        UserBO userBO = new UserBO();
+        userBO.setName("李四");
+        return userBO;
     }
 }
--- starter/src/main/java/com/wittychen/cloudmall/user/CloudMallUserStarterApplication.java
+++ starter/src/main/java/com/wittychen/cloudmall/user/CloudMallUserStarterApplication.java
@@ -3,8 +3,12 @@ package com.wittychen.cloudmall.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
 @MapperScan("com.wittychen.cloudmall.user.dal")
 public class CloudMallUserStarterApplication {
 
